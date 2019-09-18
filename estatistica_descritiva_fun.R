73270c98be2f7a519d1fa29b549a710b0aee088b
Antonio-Roberto <45764753+Antonio-Roberto@users.noreply.github.com>
2019-09-18 03:36:21 -0300

    Update estatisticas_descritivas.R
    Adicionei outros itens para análise e transformei em data.frame.

--- estatistica_descritiva_fun.R
+++ estatistica_descritiva_fun.R
@@ -1,15 +1,19 @@
 # Função estatística descritiva
 
-estatisticas <- function(x){
-  x <- x[!is.na(x)] # isso serve para que ele omita as observações com NA
+estatisticas <- function(x)EstatisticasDescritivas2estatisticas <- function(x){
+  x <- x[!is.na(x)]
   me <- mean(x)
   med <- median(x)
   n <- length(x)
   s <- sd(x)
+  cofv<- sd(x)/mean(x)
   mi <- min(x)
   ma <- max(x)
+  ampl <- ma-mi
   q25 <- quantile(x, probs = 0.25)
   q75 <- quantile(x, probs = 0.75)
-  return(c(n = n, media = me, mediana = med, desvio = s, 
-           Q = q25, Q = q75, min = mi, max = ma))
+Estatisticas<- c("N", "Média", "Mediana", "Desvio", "CV", "Quantil 25%", "Quantil 75%", "Min", "Max", "Amplitude")
+Descritivas<- c(formatC(n, digits=0), me, med, s,cofv, q25, q75, mi, ma, ampl)
+return(data.frame(Estatisticas, Descritivas, stringsAsFactors = FALSE))
 }
+print(estatisticas(sin))
